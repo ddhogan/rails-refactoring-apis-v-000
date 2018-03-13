a40abe771ff932177730ffb025a379eaca9bc5a1
Donna <donnamcla@gmail.com>
2018-03-13 18:13:12 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    github = GithubService.new
-    session[:token] = github.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
-    session[:username] = github.get_username
+    
+    session[:token] = @github.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
+    session[:username] = @github.get_username
     redirect_to '/'
   end
 end
